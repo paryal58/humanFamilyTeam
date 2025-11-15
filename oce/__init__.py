75dc12b8e78374afaa4e7dc7a77cbe4a6b2342e6
Catronater <catronater@outlook.com>
2025-11-15 11:17:36 -0500

    Add email configuration

--- oce/__init__.py
+++ oce/__init__.py
@@ -5,20 +5,32 @@ from flask import Flask
 from flask_dance.contrib.github import make_github_blueprint, github
 from flask_login import LoginManager
 from flask_session import Session
+from flask_mail import Mail
 import os
 
-
+mail = Mail()
 login_manager = LoginManager()
 password_hasher = PasswordHasher()
 
-
+import tempfile
 def create_app():
     app = Flask(__name__)
     app.config['DB_NAME'] = 'oce.db'  # TODO: extract into config file
     app.secret_key = token_urlsafe(32)  # TODO: extract into config file
     login_manager.init_app(app)
-    app.config['SESSION_TYPE'] = 'filesystem'  # Store session data in files
+    app.config['SESSION_TYPE'] = 'filesystem'
+    #app.config['SESSION_FILE_DIR'] = tempfile.mkdtemp()  # Store session data in files
     app.config['SESSION_PERMANENT'] = False  # Ensure session resets properly
+    # Flask-Mail config goes here
+    app.config.update(
+        MAIL_SERVER='smtp.gmail.com',
+        MAIL_PORT=587,
+        MAIL_USE_TLS=True,
+        MAIL_USE_SSL=False,
+        MAIL_USERNAME='noreply.catronrobotics@gmail.com',
+        MAIL_PASSWORD='',
+        MAIL_DEFAULT_SENDER='noreply.catronrobotics@gmail.com'
+    )
 
     Session(app)
     
@@ -26,6 +38,8 @@ def create_app():
     #Github OAuth config
     github_blueprint = make_github_blueprint()
     app.register_blueprint(github_blueprint, url_prefix="/github_login")
+    app.config['MAIL_DEBUG'] = True
+    mail.init_app(app)  # Initialize mail with the app
 
     from oce.accounts.routes import accounts
     from oce.content.routes import content
